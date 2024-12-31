13ae6094fb4ba42d1ec41844846bc12061ff2d54
Suriel <surielfallen@gmail.com>
2024-12-31 10:49:06 -0600

    add functions for running chat

--- ChatRunner.py
+++ ChatRunner.py
@@ -1,35 +1,46 @@
 import ollama
-import pyttsx3
-from gtts import gTTS
-from io import BytesIO
-from yapper import Yapper, PiperSpeaker, PiperVoiceUK, PiperQuality
+from yapper import Yapper, PiperSpeaker, PiperVoiceUK
 
-Marah = PiperSpeaker(
-    voice=PiperVoiceUK.ALBA
-)
-yapper = Yapper(speaker=Marah)
+messages = []
 
 def chat_loop():
-    messages = []
+    yapper = initialize_speech_to_text()
     while True:
-        user_input = input("You: ")
+        user_input=get_user_input()
         if user_input.lower() == "exit":
             break
+        append_messages("user", user_input)
 
-        messages.append({"role": "user", "content": user_input})
-
-        stream = ollama.chat(
-            model="marah",
-            messages=messages,
-            stream=True,
-        )
-        response_string = ""
-        for chunk in stream:
-            print(chunk["message"]["content"], end="", flush=True)
-            response_string += chunk.message.content
+        response_string = chat()
         print()
         yapper.yap(response_string, plain=True)
-        messages.append({"role": "assistant", "content": response_string})
+        append_messages("assistant", response_string)
+
+
+def initialize_speech_to_text():
+    return Yapper(
+        speaker = PiperSpeaker(
+            voice = PiperVoiceUK.ALBA
+        )
+    )
+
+def get_user_input():
+    return input("You: ")
+
+def chat():
+    stream = ollama.chat(
+        model="marah",
+        messages=messages,
+        stream=True,
+    )
+    response_string = ""
+    for chunk in stream:
+        print(chunk["message"]["content"], end="", flush=True)
+        response_string += chunk.message.content
+    return response_string
+
+def append_messages(role,message):
+    messages.append({"role": role, "content": message})
 
 if __name__ == "__main__":
     chat_loop()
